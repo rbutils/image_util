81520c6efc8f599d40a3e10c8f5b71ba858065c0
hmdne <54514036+hmdne@users.noreply.github.com>
2025-07-20 17:26:03 +0200

    Add background filter

---
+++ spec/filter/background_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe ImageUtil::Image do
+  describe '#background' do
+    it 'returns self for images without alpha' do
+      img = described_class.new(1, 1, color_length: 3) { ImageUtil::Color[1, 2, 3] }
+      img.background(ImageUtil::Color[0]).should equal(img)
+    end
+
+    it 'applies background color for images with alpha' do
+      img = described_class.new(1, 1) { ImageUtil::Color[255, 0, 0, 128] }
+      out = img.background(ImageUtil::Color[0, 0, 255])
+      out.should_not equal(img)
+      out.color_length.should == 3
+      out[0, 0].should == ImageUtil::Color.new(128.0, 0.0, 127.0)
+    end
+  end
+end

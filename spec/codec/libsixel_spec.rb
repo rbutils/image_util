5d657773dd7c19a3cc2aae3779a1de552b647f36
hmdne <54514036+hmdne@users.noreply.github.com>
2025-07-21 08:05:59 +0200

    Add Libsixel encoder and CI setup

---
+++ spec/codec/libsixel_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+RSpec.describe ImageUtil::Codec::Libsixel do
+  before do
+    skip 'libsixel not available' unless described_class.supported?
+  end
+
+  it 'encodes an image to sixel' do
+    img = ImageUtil::Image.new(2, 1) { |x,_| ImageUtil::Color[x * 255, 0, 0] }
+    data = described_class.encode(:sixel, img)
+    data.start_with?("\ePq").should be true
+    data.end_with?("\e\\").should be true
+  end
+
+  it 'raises for unsupported format' do
+    img = ImageUtil::Image.new(1,1)
+    ->{ described_class.encode(:foo, img) }.should raise_error(ImageUtil::Codec::UnsupportedFormatError)
+  end
+end

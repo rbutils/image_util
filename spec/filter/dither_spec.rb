f638490fd520fa1847f5520b2eefc8e3b49b872d
hmdne <54514036+hmdne@users.noreply.github.com>
2025-07-21 05:40:32 +0200

    Refactor specs to use Statistic::Color

--- spec/filter/dither_spec.rb
+++ spec/filter/dither_spec.rb
@@ -5,9 +5,7 @@ RSpec.describe ImageUtil::Image do
     it 'reduces colors in place' do
       img = described_class.new(4,1) { |x,_| ImageUtil::Color[x*50,0,0] }
       img.dither!(2)
-      colors = []
-      img.each_pixel { |c| colors << c.to_a }
-      colors.uniq.length.should <= 2
+      img.unique_color_count.should <= 2
     end
   end
 
@@ -16,12 +14,8 @@ RSpec.describe ImageUtil::Image do
       img = described_class.new(2,1) { |x,_| ImageUtil::Color[x*100,0,0] }
       dup = img.dither(1)
       dup.should_not equal(img)
-      orig_colors = []
-      img.each_pixel { |c| orig_colors << c.to_a }
-      new_colors = []
-      dup.each_pixel { |c| new_colors << c.to_a }
-      orig_colors.uniq.length.should == 2
-      new_colors.uniq.length.should == 1
+      img.unique_color_count.should == 2
+      dup.unique_color_count.should == 1
     end
   end
 end

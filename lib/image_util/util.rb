64ac155675ac358bff63f4eb58ca33b4bda704fe
hmdne <54514036+hmdne@users.noreply.github.com>
2025-07-21 10:16:09 +0200

    Refactor Util to static IRB setup

--- lib/image_util/util.rb
+++ lib/image_util/util.rb
@@ -4,24 +4,12 @@ module ImageUtil
   module Util
     module_function
 
-    # FIXME: Doesn't really work. Temporary partial solution.
-    def unlock_irb(p)
-      begin
-        old_pager = IRB.conf[:USE_PAGER]
-        IRB.conf[:USE_PAGER] = false
-        old_echo = IRB.CurrentContext.echo_on_assignment
-        IRB.CurrentContext.echo_on_assignment = true
-      rescue StandardError
-        # If IRB is not present, we don't apply this hack.
-        yield
-        return
-      end
-      yield
-      ObjectSpace.define_finalizer(p, lambda do |_id|
-        IRB.conf[:USE_PAGER] = old_pager
-        IRB.CurrentContext.echo_on_assignment = old_echo
-      rescue StandardError
-      end)
+    # Applies configuration tweaks for IRB, if available.
+    def irb_fixup
+      IRB.conf[:USE_PAGER] = false
+      IRB.CurrentContext.echo_on_assignment = true
+    rescue StandardError
+      nil
     end
   end
 end

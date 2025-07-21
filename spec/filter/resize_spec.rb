d7930f2b2d17f2acef6dc81b975e17df72eea629
hmdne <54514036+hmdne@users.noreply.github.com>
2025-07-21 11:04:51 +0200

    Update resize filter for N-dimensions

--- spec/filter/resize_spec.rb
+++ spec/filter/resize_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe ImageUtil::Image do
       out = img.resize(2, 2, view: ImageUtil::View::Rounded)
       out.width.should == 2
       out.height.should == 2
-      out[1,1].should == ImageUtil::Color[1]
+      out[1, 1].should == ImageUtil::Color[1]
       img.width.should == 1
     end
 
@@ -17,6 +17,13 @@ RSpec.describe ImageUtil::Image do
       color = out[1,1]
       color.r.should be_within(0.1).of(50)
       color.g.should be_within(0.1).of(50)
+    end
+
+    it 'scales images with more than two dimensions' do
+      img = described_class.new(1, 1, 2) { |_, _, z| ImageUtil::Color[z * 100] }
+      out = img.resize(1, 1, 3)
+      out.dimensions.should == [1, 1, 3]
+      out[0, 0, 1].r.should be_within(0.1).of(50)
     end
   end
 end

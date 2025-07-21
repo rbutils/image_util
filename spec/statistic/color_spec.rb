534815f1d6bb445cf885f5278f59758188cd8c70
hmdne <54514036+hmdne@users.noreply.github.com>
2025-07-21 05:40:32 +0200

    Refactor specs to use Statistic::Color

---
+++ spec/statistic/color_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+RSpec.describe ImageUtil::Statistic::Color do
+  let(:image) do
+    ImageUtil::Image.new(2, 1) { |x, _y| ImageUtil::Color[x, 0, 0] }
+  end
+
+  it 'builds a histogram of pixel colors' do
+    image.histogram.should == {
+      ImageUtil::Color[0, 0, 0, 255] => 1,
+      ImageUtil::Color[1, 0, 0, 255] => 1
+    }
+  end
+
+  it 'returns unique colors' do
+    image.unique_colors.should match_array([
+      ImageUtil::Color[0, 0, 0, 255],
+      ImageUtil::Color[1, 0, 0, 255]
+    ])
+  end
+
+  it 'counts unique colors' do
+    image.unique_color_count.should == 2
+  end
+end

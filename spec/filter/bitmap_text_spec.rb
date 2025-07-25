56ffdb59afa306b1afd4ad2c5209a69bf56f8d3b
hmdne <54514036+hmdne@users.noreply.github.com>
2025-07-25 15:18:59 +0200

    Make bitmap_text filter work with multidimensional images

--- spec/filter/bitmap_text_spec.rb
+++ spec/filter/bitmap_text_spec.rb
@@ -11,6 +11,17 @@ RSpec.describe ImageUtil::Image do
         base[x + 2, y + 3].should == text[x, y]
       end
     end
+
+    it 'accepts coordinates for extra dimensions' do
+      base = described_class.new(16, 16, 2) { ImageUtil::Color[0] }
+      font = ImageUtil::BitmapFont.default_font
+      text = described_class.bitmap_text('A', font: font)
+      base.bitmap_text!('A', 1, 1, 1, font: font)
+      text.each_pixel_location do |(x, y)|
+        base[x + 1, y + 1, 1].should == text[x, y]
+      end
+      base[1, 1, 0].should == ImageUtil::Color[0]
+    end
   end
 
   describe '#bitmap_text' do
